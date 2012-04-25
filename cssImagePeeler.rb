76b61f9b95962be00b35308b4cc36b58856aa7ba
P.S.V.R_linode <pmq2001@gmail.com>
2012-04-25 17:28:53 +0800

    more rubost

--- cssImagePeeler.rb
+++ cssImagePeeler.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 unless 2==ARGV.count
   puts "Usage:"
   puts "  cssImagePeeler.rb CSS_FILE-NAME BASE_URL"
@@ -8,14 +6,21 @@ end
 
 File.open(ARGV[0]) do |f|
   while(line = f.gets)
-    if line =~ /url\(['"]*(.+)['"]*\)/
-      p line
+    if line =~ /url\(['"]*([^('")]+)['"]*\)/
       now = String.new $1
+heads = ARGV[1].split('://')
+heads[1] = heads[1].split('/')[0]
+tails = ARGV[1].split('://')[-1].split('/')[1..-1]
+after = '/'+tails.join('/')+'/'
       if(now=~/^http:\/\//)
-        p `curl -O #{now}`
+	path="#{now}"
       else
-        p `curl -O #{ARGV[1]}#{now}`
-     end
+	path="#{after}#{now}"
+      end
+path=File.expand_path(path)
+url = "#{heads[0]}://#{heads[1]}#{path}"
+      p cmd="curl -O #{url}"
+      p `#{cmd}`
     end
   end
 end
